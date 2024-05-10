73919572612b5bbae0c61b6153fa903d9c25be6d
Ishaaq-dev <i.iqbalwork@gmail.com>
2024-05-10 19:02:08 +0100

    corrects unique identifier

--- src/com.ishaaq-dev.govee/app.js
+++ src/com.ishaaq-dev.govee/app.js
@@ -1,7 +1,7 @@
 /// <reference path="libs/js/action.js" />
 /// <reference path="libs/js/stream-deck.js" />
 
-const myAction = new Action('com.ishaaq_dev.action');
+const myAction = new Action('com.ishaaq_dev.govee.action');
 
 /**
  * The first event fired when Stream Deck starts
