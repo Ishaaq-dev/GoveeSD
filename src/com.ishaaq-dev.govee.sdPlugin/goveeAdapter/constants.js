d065065c842dfda7b367e890b3b91a25670f96bb
Ishaaq-dev <i.iqbalwork@gmail.com>
2024-05-13 20:44:05 +0100

    adds urls

---
+++ src/com.ishaaq-dev.govee.sdPlugin/goveeAdapter/constants.js
@@ -0,0 +1,13 @@
+const GOVEE_URL = 'https://openapi.api.govee.com';
+const V1 = `${GOVEE_URL}/router/api/v1`;
+
+export const URLS = {
+    GOVEE_URL,
+    GET: {
+        DEVICES: `${V1}/user/devices`,
+        DEVICE_STATE: `${V1}/device/state`
+    },
+    POST: {
+        DEVICE_CONTROL: `${V1}/device/scenes`
+    }
+}
