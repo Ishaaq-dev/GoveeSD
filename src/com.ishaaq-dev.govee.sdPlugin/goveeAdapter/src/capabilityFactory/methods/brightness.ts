465e146eb7da3936805b59949fe170d435b0b230
Ishaaq Iqbal <i.iqbalwork@gmail.com>
2024-08-10 22:18:16 +0100

    creates bright request method in factoy

---
+++ src/com.ishaaq-dev.govee.sdPlugin/goveeAdapter/src/capabilityFactory/methods/brightness.ts
@@ -0,0 +1,10 @@
+import { Device } from "../../interfaces/responses";
+import { CAPABILITIES, createRequest } from "./constants";
+
+export function createBrightnessRequest(device: Device, value: number) {
+    if (value < 0) value = 0;
+    if (value > 100) value = 100;
+    const capability = CAPABILITIES.SET_BRIGHTNESS;
+    capability.value = value;
+    return createRequest(device.sku, device.device, capability);
+}
